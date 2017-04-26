95ca749127ca0cdb93edbf4abaf4cd7463692a01
JuliusCesarEstrope <juliuscesarme@gmail.com>
2017-04-26 15:12:46 -0400

    Modified Encoder class

--- src/main/Pi.java
+++ src/main/Pi.java
@@ -20,8 +20,8 @@ public class Pi {
         // provision gpio pin #01 as an output pin and turn off
         final GpioPinDigitalOutput pin0 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MotorForwardChannel", PinState.LOW);
         final GpioPinDigitalOutput pin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MotorReverseChannel", PinState.LOW);
-        final GpioPinDigitalInput pin2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "EncoderChannel1");
-        final GpioPinDigitalInput pin3 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03, "EncoderChannel2");
+        final GpioPinDigitalInput pin2 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "TACH00");
+        final GpioPinDigitalInput pin3 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_03, "TACH01");
         
 		Encoder encoder = new Encoder(pin2, pin3);
 		
@@ -65,6 +65,7 @@ public class Pi {
         // stop all GPIO activity/threads by shutting down the GPIO controller
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
         gpio.shutdown();
+        encoder.stop();
 
         System.out.println("End of transmission");
 
--- src/sensors/Encoder.java
+++ src/sensors/Encoder.java
@@ -12,42 +12,52 @@ public class Encoder implements Runnable{
 	boolean running;
 
 	public Encoder(GpioPinDigitalInput pin1, GpioPinDigitalInput pin2) {
+		System.out.println("Created an Encoder");
 		this.pin1 = pin1;
 		this.pin2 = pin2;
 		
 		count = 0;
 		encoderThread = new Thread(this);
-		encoderThread.start();
+		start();
 	}
 	
 	public void start(){
+		System.out.println("Ran Start");
 		running = true;
 		if(!encoderThread.isAlive())
 			encoderThread.start();
+		System.out.println("Ended Start");
 	}
 	
 	public int get(){
+		System.out.println("Ran Get");
 		return count;
 	}
 	
 	public void reset(){
+		System.out.println("Ran Reset");
 		count = 0;
 	}
 	
 	public void stop(){
+		System.out.println("Ran Stop");
 		running = false;
 	}
 
 	public void run() {
-		boolean prevStatePin1 = false, prevStatePin2 = false;
+		boolean prevStatePin1 = false, prevStatePin2 = false, beenFalse = true;
 		while(running){
-			if(pin1.isHigh() && pin2.isHigh() && !(prevStatePin2))
-				count++;
-			else if(pin1.isHigh() && pin2.isHigh() && !(prevStatePin1))
-				count--;
-				
-			prevStatePin1 = pin1.isHigh();
-			prevStatePin2 = pin2.isHigh();
+			if(beenFalse){
+				if(pin1.isHigh() && pin2.isHigh() && !(prevStatePin2)){
+					beenFalse = false;
+					count++;
+				}
+				else if(pin1.isHigh() && pin2.isHigh() && !(prevStatePin1)){
+					beenFalse = false;
+					count--;
+				}
+			} else if (pin1.isLow() && pin2.isLow())
+				beenFalse = true;
 		}
 	}
 
